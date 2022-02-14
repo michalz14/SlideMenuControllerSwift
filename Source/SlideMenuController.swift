7409ca9b7fb9c765a5c2cb3a9eef84d5d24dd9a6
Michał Zieliński <mzielinski.office@gmail.com>
2022-02-14 15:32:50 +0100

    Support main window for below iOS 13 version

--- Source/SlideMenuController.swift
+++ Source/SlideMenuController.swift
@@ -1093,11 +1093,15 @@ extension UIViewController {
 extension UIApplication {
     
     var mainWindow: UIWindow? {
-        UIApplication.shared.connectedScenes
+        if #available(iOS 13, *) {
+            return UIApplication.shared.connectedScenes
                 .filter({$0.activationState == .foregroundActive})
                 .compactMap({$0 as? UIWindowScene})
                 .first?.windows
                 .filter({$0.isKeyWindow}).first
+        } else {
+            return UIApplication.shared.keyWindow
+        }
     }
     
 }
